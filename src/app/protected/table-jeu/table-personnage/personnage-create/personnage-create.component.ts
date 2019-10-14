489062a4daa03e58d78c74ae16d6bf84ab718857
Brocel <geenens.prof@gmail.com>
2019-10-14 18:07:18 +0200

    edit de creation sur le modele de profil

--- src/app/protected/table-jeu/table-personnage/personnage-create/personnage-create.component.ts
+++ src/app/protected/table-jeu/table-personnage/personnage-create/personnage-create.component.ts
@@ -11,5 +11,18 @@ export class PersonnageCreateComponent implements OnInit {
 
   ngOnInit() {
   }
+  createColumns = ['propriete', 'valeur'];
+  dataCreate = CREATE_DATA;
+  }
+  /*Profil : statistiques*/
+  export interface CreateProfil {
+    propriete: string;
+    valeur: string;
+  }
 
-}
+  const CREATE_DATA: CreateProfil[] = [
+    {propriete: 'Créer un joueur', valeur: 'todo btn vers component'},
+    {propriete: 'Créer un Mdj', valeur: 'todo btn vers component'},
+    {propriete: 'Recherche', valeur: 'todo component'},
+    {propriete: 'Invitation', valeur: '1'},
+  ];
