063d629ea3b6e2a06f7cd184ab8aefb964c0d304
Brocel <geenens.prof@gmail.com>
2019-10-14 11:03:14 +0200

    edit profil2

--- src/app/protected/table-jeu/table-profil/profil-stats/profil-stats.component.ts
+++ src/app/protected/table-jeu/table-profil/profil-stats/profil-stats.component.ts
@@ -13,14 +13,24 @@ export class ProfilStatsComponent implements OnInit {
   }
   infoColumns = ['propriete', 'valeur'];
   dataInfo = INFO_DATA;
+  dataStats = STATS_DATA;
   }
-
+/*Profil : informations*/
   export interface InfoProfil {
     propriete: string;
     valeur: number;
   }
 
   const INFO_DATA: InfoProfil[] = [
+    {propriete: 'E-mail', valeur: 'userEmail'},
+    {propriete: 'prop2', valeur: 'val2'},
+    {propriete: 'prop3', valeur: 'val3'},
+    {propriete: 'prop4', valeur: 'val4'},
+  ];
+
+/*Profil : statistiques*/
+
+  const STATS_DATA: InfoProfil[] = [
     {propriete: 'E-mail', valeur: 'userEmail'},
     {propriete: 'prop2', valeur: 'val2'},
     {propriete: 'prop3', valeur: 'val3'},
