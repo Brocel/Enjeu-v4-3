3bcb14f867b1c69ae93b655cb0fe746abc05825d
Brocel <geenens.prof@gmail.com>
2019-10-15 10:01:04 +0200

    ajout de table-aff et déplacement de table chat dedans

---
+++ src/app/protected/table-jeu/table-affichage/table-affichage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'enjeu-table-affichage',
+  templateUrl: './table-affichage.component.html',
+  styleUrls: ['./table-affichage.component.scss']
+})
+export class TableAffichageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/protected/table-jeu/table-jeu.module.ts
+++ src/app/protected/table-jeu/table-jeu.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { TableJeuComponent } from './table-jeu/table-jeu.component';
-import { TableChatComponent } from './table-chat/table-chat.component';
 import { TableProfilComponent } from './table-profil/table-profil.component';
 import { TablePersonnageComponent } from './table-personnage/table-personnage.component';
 import { TableAventureComponent } from './table-aventure/table-aventure.component';
 import { TableDonjonComponent } from './table-donjon/table-donjon.component';
 import { TableTutoComponent } from './table-tuto/table-tuto.component';
-import { TableHeadComponent } from './table-head/table-head.component';
 import { ProfilStatsComponent } from './table-profil/profil-stats/profil-stats.component';
 import { ProfilInfosComponent } from './table-profil/profil-infos/profil-infos.component';
 import { ProfilFriendsComponent } from './table-profil/profil-friends/profil-friends.component';
 import { PersonnageMainComponent } from './table-personnage/personnage-main/personnage-main.component';
 import { PersonnageMenuComponent } from './table-personnage/personnage-menu/personnage-menu.component';
 import { PersonnageCreateComponent } from './table-personnage/personnage-create/personnage-create.component';
+import { TableAffichageComponent } from './table-affichage/table-affichage.component';
+import { TableChatComponent } from './table-affichage/table-chat/table-chat.component';
 
 
 
 @NgModule({
-  declarations: [TableJeuComponent, TableChatComponent, TableProfilComponent, TablePersonnageComponent, TableAventureComponent, TableDonjonComponent, TableTutoComponent, TableHeadComponent, ProfilStatsComponent, ProfilInfosComponent, ProfilFriendsComponent, PersonnageMainComponent, PersonnageMenuComponent, PersonnageCreateComponent],
+  declarations: [TableJeuComponent, TableChatComponent, TableProfilComponent, TablePersonnageComponent, TableAventureComponent, TableDonjonComponent, TableTutoComponent, ProfilStatsComponent, ProfilInfosComponent, ProfilFriendsComponent, PersonnageMainComponent, PersonnageMenuComponent, PersonnageCreateComponent, TableAffichageComponent],
   imports: [
     SharedModule
   ]
