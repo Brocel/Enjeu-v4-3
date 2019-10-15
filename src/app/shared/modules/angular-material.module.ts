373659bd03b807af63b42be43bf8eb941c7edc61
Brocel <geenens.prof@gmail.com>
2019-10-15 15:40:00 +0200

    edit styles des éléments de table-jeu raz

--- src/app/shared/modules/angular-material.module.ts
+++ src/app/shared/modules/angular-material.module.ts
@@ -3,10 +3,11 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatTableModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
 import { MatDividerModule } from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-    imports: [MatTabsModule, MatTableModule, MatCardModule, MatDividerModule],
-    exports: [MatTabsModule, MatTableModule, MatCardModule, MatDividerModule]
+    imports: [MatTabsModule, MatTableModule, MatCardModule, MatDividerModule, MatButtonModule],
+    exports: [MatTabsModule, MatTableModule, MatCardModule, MatDividerModule, MatButtonModule]
 })
 
 export class AngularMaterialModule { }
