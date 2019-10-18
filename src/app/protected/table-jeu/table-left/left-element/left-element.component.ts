08125822931f18e452d02a1b0a2b1d34f2b0bdd5
Brocel <geenens.prof@gmail.com>
2019-10-18 15:40:21 +0200

    edit left element2 preparation de l'affichage de comp dumb/smrtart 3

--- src/app/protected/table-jeu/table-left/left-element/left-element.component.ts
+++ src/app/protected/table-jeu/table-left/left-element/left-element.component.ts
@@ -22,6 +22,7 @@ export interface dataModel {
   propriete: string;
   valeur: string;
 }
+/*Profil component*/
 const PROFIL_INFO_DATA: dataModel[] = [
   {propriete: 'e-mail', valeur: 'test@test2.com'},
   {propriete: 'ville', valeur: 'Test City'},
@@ -34,9 +35,28 @@ const PROFIL_STATS_DATA: dataModel[] = [
   {propriete: 'pays', valeur: 'France'},
   {propriete: 'dispo', valeur: 'todo'},
 ];
-const FRIENDS_DATA: dataModel[] = [
+const PROFIL_FRIENDS_DATA: dataModel[] = [
   {propriete: 'Nbr Compagnons', valeur: '0'},
   {propriete: 'Liste', valeur: 'todo component'},
+  {propriete: 'Recherche', valeur: 'todo component'},
+  {propriete: 'Invitation', valeur: '1'},
+];
+/*Stat component*/
+const PERSO_MAIN_DATA: dataModel[] = [
+  {propriete: 'niveau', valeur: '1'},
+  {propriete: 'race', valeur: 'humain'},
+  {propriete: 'sexe', valeur: 'homme'},
+  {propriete: 'emploi', valeur: 'metier + spé'},
+];
+const PERSO_ACTION_DATA: dataModel[] = [
+  {propriete: 'Rôle', valeur: 'MdJ/Joueur'},
+  {propriete: 'select perso main', valeur: 'todo liste'},
+  {propriete: 'detail perso', valeur: 'liste'},
+  {propriete: 'suprrimer', valeur: 'liste'},
+];
+const PERSO_CREATION_DATA: dataModel[] = [
+  {propriete: 'Créer un joueur', valeur: 'todo btn vers component'},
+  {propriete: 'Créer un Mdj', valeur: 'todo btn vers component'},
   {propriete: 'Recherche', valeur: 'todo component'},
   {propriete: 'Invitation', valeur: '1'},
 ];
