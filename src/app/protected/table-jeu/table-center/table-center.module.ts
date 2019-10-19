0f2a2ec6989666aa83b336b63b8da0036f2525f4
Brocel <geenens.prof@gmail.com>
2019-10-19 13:20:56 +0200

    refonte des modules et factorisation table-center

--- src/app/protected/table-jeu/table-center/table-center.module.ts
+++ src/app/protected/table-jeu/table-center/table-center.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
-
+import { TableCenterComponent } from './table-center/table-center.component';
+import { TableHeadComponent } from './table-head/table-head.component';
+import { TableAffichageComponent } from './table-affichage/table-affichage.component';
+import { AffichageChatComponent } from './table-affichage/affichage-chat/affichage-chat.component';
+import { AffichageCreateComponent } from './table-affichage/affichage-create/affichage-create.component';
+import { AffichageDetailsComponent } from './table-affichage/affichage-details/affichage-details.component';
+import { AffichageGameComponent } from './table-affichage/affichage-game/affichage-game.component';
+import { AffichageDefautComponent } from './table-affichage/affichage-defaut/affichage-defaut.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TableCenterComponent,
+    TableHeadComponent,
+    TableAffichageComponent,
+    AffichageChatComponent,
+    AffichageCreateComponent,
+    AffichageDetailsComponent,
+    AffichageGameComponent,
+    AffichageDefautComponent
+  ],
   imports: [
     SharedModule
+  ],
+  exports: [
+    TableCenterComponent
   ]
 })
 export class TableCenterModule { }
--- src/app/protected/table-jeu/table-jeu.module.ts
+++ src/app/protected/table-jeu/table-jeu.module.ts
@@ -4,14 +4,7 @@ import { TableJeuComponent } from './table-jeu/table-jeu.component';
 /*module de Left*/
 import { TableLeftModule } from './table-left/table-left.module';
 /*module de Center*/
-import { TableCenterComponent } from './table-center/table-center.component';
-import { TableHeadComponent } from './table-center/table-head/table-head.component';
-import { TableAffichageComponent } from './table-center/table-affichage/table-affichage.component';
-import { AffichageChatComponent } from './table-center/table-affichage/affichage-chat/affichage-chat.component';
-import { AffichageCreateComponent } from './table-center/table-affichage/affichage-create/affichage-create.component';
-import { AffichageDetailsComponent } from './table-center/table-affichage/affichage-details/affichage-details.component';
-import { AffichageGameComponent } from './table-center/table-affichage/affichage-game/affichage-game.component';
-import { AffichageDefautComponent } from './table-center/table-affichage/affichage-defaut/affichage-defaut.component';
+
 /*module de Right*/
 import { TableRightComponent } from './table-right/table-right.component';
 import { RightElementComponent } from './table-right/right-element/right-element.component';
@@ -20,7 +13,7 @@ import { TableRightModule } from './table-right/table-right.module';
 import { TableCenterModule } from './table-center/table-center.module';
 
 @NgModule({
-  declarations: [TableJeuComponent, TableAffichageComponent, AffichageChatComponent, AffichageCreateComponent, AffichageDetailsComponent, AffichageGameComponent, AffichageDefautComponent, TableCenterComponent, TableRightComponent, TableHeadComponent, RightElementComponent, RightFeatureComponent],
+  declarations: [TableJeuComponent, TableRightComponent, RightElementComponent, RightFeatureComponent],
   imports: [
     SharedModule,
     TableLeftModule,
