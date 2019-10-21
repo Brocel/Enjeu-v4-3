d198d65ac9262a1f57607aa17818ab8417e5eed5
Brocel <geenens.prof@gmail.com>
2019-10-21 13:55:53 +0200

    routing des comps de center

--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -13,6 +13,11 @@ import { LeftElementComponent } from './table-jeu/table-left/left-element/left-e
 import { TableCenterComponent } from './table-jeu/table-center/table-center/table-center.component';
 import { TableHeadComponent } from './table-jeu/table-center/table-head/table-head.component';
 import { TableAffichageComponent } from './table-jeu/table-center/table-affichage/table-affichage/table-affichage.component';
+import { AffichageChatComponent } from './table-jeu/table-center/table-affichage/affichage-chat/affichage-chat.component';
+import { AffichageCreateComponent } from './table-jeu/table-center/table-affichage/affichage-create/affichage-create.component';
+import { AffichageDefautComponent } from './table-jeu/table-center/table-affichage/affichage-defaut/affichage-defaut.component';
+import { AffichageDetailsComponent } from './table-jeu/table-center/table-affichage/affichage-details/affichage-details.component';
+import { AffichageGameComponent } from './table-jeu/table-center/table-affichage/affichage-game/affichage-game.component';
 /*Right*/
 import { TableRightComponent } from './table-jeu/table-right/table-right/table-right.component';
 import { RightFeatureComponent } from './table-jeu/table-right/right-feature/right-feature.component';
@@ -27,7 +32,14 @@ const routes: Routes = [
   //routes de Center :
   { path: 'protected/table-jeu/table-center', canActivate: [AuthGuardService], component: TableCenterComponent },
   { path: 'protected/table-jeu/table-center/table-head', canActivate: [AuthGuardService], component: TableHeadComponent },
+    //routes de affichage
   { path: 'protected/table-jeu/table-center/table-affichage', canActivate: [AuthGuardService], component: TableAffichageComponent },
+  { path: 'protected/table-jeu/table-center/table-affichage/aff-chat', canActivate: [AuthGuardService], component: AffichageChatComponent },
+  { path: 'protected/table-jeu/table-center/table-affichage/aff-create', canActivate: [AuthGuardService], component: AffichageCreateComponent },
+  { path: 'protected/table-jeu/table-center/table-affichage/aff-defaut', canActivate: [AuthGuardService], component: AffichageDefautComponent },
+  { path: 'protected/table-jeu/table-center/table-affichage/aff-details', canActivate: [AuthGuardService], component: AffichageDetailsComponent },
+  { path: 'protected/table-jeu/table-center/table-affichage/aff-game', canActivate: [AuthGuardService], component: AffichageGameComponent },
+
   //routes de Right :
   { path: 'protected/table-jeu/table-right', canActivate: [AuthGuardService], component: TableRightComponent },
   { path: 'protected/table-jeu/table-right/right-feature', canActivate: [AuthGuardService], component: RightFeatureComponent },
